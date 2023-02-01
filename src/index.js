5ea6fd34c229b65ea3678dd7ae485504d55565d4
Federico Luna <fluna1973@yahoo.com>
2023-02-01 11:09:33 -0600

    add first projects

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import "swiper/css/bundle";
 import NavBar from './Components/NavBar/NavBar';
 
 const container = document.getElementById('root');
