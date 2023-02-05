e04f35ce755d32acfef9c43279de1ac819f222ef
Federico Luna <fluna1973@yahoo.com>
2023-02-05 11:54:23 -0600

    add coworkers names

--- src/Components/Stack/Images/Imgarray.jsx
+++ src/Components/Stack/Images/Imgarray.jsx
@@ -12,17 +12,17 @@ const radioImgs = [
   },
   {
     pic: 'https://media.licdn.com/dms/image/D4D03AQGDm50BK1uaPA/profile-displayphoto-shrink_100_100/0/1664381209287?e=1681344000&v=beta&t=cPdj7x15KRIY8YMbeIPKtOq_Lwhnj7xIwpgazBeQGAo',
-    name: 'React',
+    name: 'Addis Tsega',
     desclink: 'We\'ve joined our hands on several projects, and Federico is one of the best people I had as a partner. I highly recommend his expertise to any organization looking for a Full-stack developer. He is the most profound person I have met, and his ability to tackle any problem is remarkable and with a warm smile. Federico would become an appreciated member of any team.',
   },
   {
     pic: 'https://media.licdn.com/dms/image/C4D03AQG1-SNI18A4_g/profile-displayphoto-shrink_100_100/0/1663930572336?e=1681344000&v=beta&t=dbDXr6h2NsMRgx9M6B5-LkJfdN__d-XDZ18vWQpoGA4',
-    name: 'React',
+    name: 'Abdelrhman Samy',
     desclink :'"Federico is one of the brightest software developers I\'ve ever met. I worked alongside him in Microverse, and during that time he proved his commitment and determination over and over again. He is always challenging himself--he\'s the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I\'ve worked with him on. He\'s also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend him without reservation."',
   },
   {
     pic: 'https://media.licdn.com/dms/image/D4D03AQFuvBfoxQQK4Q/profile-displayphoto-shrink_100_100/0/1665264186231?e=1681344000&v=beta&t=_r5tRFKaynKHhgepbzFSrEIYRJIMIGgKhVBmOeNXmuc',
-    name: 'React',
+    name: 'Fabio Lima',
     desclink: '"I got the opportunity to learn and speak with Federico on several occasions. He is knowledgeable, diligent, and smart. He makes every attempt to assist others while also seeking to learn from his colleagues whenever possible. His commitment and motivation are admirable since he is constantly looking for opportunities to grow and learn new things. Federico is the sort of individual that strives to excel and would be a wonderful addition to any team."',
   },
 ];
