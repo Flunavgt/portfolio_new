b783f4b1df2d9f84f4322c420ae89feea7651cd9
Federico Luna <fluna1973@yahoo.com>
2023-02-05 11:00:28 -0600

    testimonial set up

--- src/Components/Stack/Images/Imgarray.jsx
+++ src/Components/Stack/Images/Imgarray.jsx
@@ -1,17 +1,30 @@
-import img1 from './ppp.png';
-import img2 from './ppp2.png';
-import img3 from './ppp.png';
-import img4 from './ppp.png';
-import img5 from './ppp.png';
-import img6 from './ppp.png';
 
 const radioImgs = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-]
+  {
+    pic: 'https://media.licdn.com/dms/image/D5635AQE3gU3fE3tvaw/profile-framedphoto-shrink_100_100/0/1674576313436?e=1676221200&v=beta&t=kcwvkBXLY4UNEK1jJYlA20GO-N5vTccF7CC-lzQodnA',
+    name: 'Erik Sánchez Beltrán',
+    desclink :'"I had the opportunity to participate with Federico at the beginning of Microverse, I discovered his excellent attitude along with many qualities that are not seen very often nowadays. Commitment, loyal to his ideals, a great resolution capacity, and effort. All this added to his kindness, and patience motivates me to highly recommend him. He is someone who doesn\'t give up, his problem-solving skills by investigating paired with his excellent eye for detail make him a unique programmer. It doesn\'t matter if he doesn\'t know a subject, give him a second and he\'ll have everything under control. Without a doubt, Federico must be part of your team."',
+  },
+  {
+    pic: 'https://media.licdn.com/dms/image/C4D03AQHr8oxvzo7IQg/profile-displayphoto-shrink_100_100/0/1663863384390?e=1681344000&v=beta&t=YIR4rdni3Xih7NOubyx0BxcdxWNEyYI6PZh3ftFysfE',
+    name: 'Luis David Rodriguez Valades',
+    desclink :'"I have had the pleasure of working with Federico for more than 6 months. After many projects together, working tirelessly, I\'m sure his knowledge, skills, and moreover, his willingness to learn, help, and finish every project have been beneficial to the team. He is self-disciplined, passionate about his work, creative, and diligent. Not only will you realize that hiring Federico is more than an eloquent decision, but you will also discover that my words are not enough to portray the human being and human quality that your company and team will have. In a few words, I have not been the same after having met Federico."',
+  },
+  {
+    pic: 'https://media.licdn.com/dms/image/D4D03AQGDm50BK1uaPA/profile-displayphoto-shrink_100_100/0/1664381209287?e=1681344000&v=beta&t=cPdj7x15KRIY8YMbeIPKtOq_Lwhnj7xIwpgazBeQGAo',
+    name: 'React',
+    desclink: 'We\'ve joined our hands on several projects, and Federico is one of the best people I had as a partner. I highly recommend his expertise to any organization looking for a Full-stack developer. He is the most profound person I have met, and his ability to tackle any problem is remarkable and with a warm smile. Federico would become an appreciated member of any team.',
+  },
+  {
+    pic: 'https://media.licdn.com/dms/image/C4D03AQG1-SNI18A4_g/profile-displayphoto-shrink_100_100/0/1663930572336?e=1681344000&v=beta&t=dbDXr6h2NsMRgx9M6B5-LkJfdN__d-XDZ18vWQpoGA4',
+    name: 'React',
+    desclink :'"Federico is one of the brightest software developers I\'ve ever met. I worked alongside him in Microverse, and during that time he proved his commitment and determination over and over again. He is always challenging himself--he\'s the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I\'ve worked with him on. He\'s also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend him without reservation."',
+  },
+  {
+    pic: 'https://media.licdn.com/dms/image/D4D03AQFuvBfoxQQK4Q/profile-displayphoto-shrink_100_100/0/1665264186231?e=1681344000&v=beta&t=_r5tRFKaynKHhgepbzFSrEIYRJIMIGgKhVBmOeNXmuc',
+    name: 'React',
+    desclink: '"I got the opportunity to learn and speak with Federico on several occasions. He is knowledgeable, diligent, and smart. He makes every attempt to assist others while also seeking to learn from his colleagues whenever possible. His commitment and motivation are admirable since he is constantly looking for opportunities to grow and learn new things. Federico is the sort of individual that strives to excel and would be a wonderful addition to any team."',
+  },
+];
 
 export default radioImgs;
--- src/Components/Stack/Stack.jsx
+++ src/Components/Stack/Stack.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import radioImgs from "./Images/Imgarray";
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import radioImgs from './Images/Imgarray';
 
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
+import './Stack.css';
 
-import "./Stack.css";
-
-import { FreeMode, Navigation, Thumbs } from "swiper";
+import { FreeMode, Navigation, Thumbs } from 'swiper';
 
 const Stack = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <div  id="Stack">
+    <div id='Stack'>
       <Swiper
         style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
         }}
         spaceBetween={10}
         navigation={true}
         thumbs={{ swiper: thumbsSwiper }}
         modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >{radioImgs.map((img, i) => {
-        return (
-          <SwiperSlide key={i}>
-            <img src={img} alt=""></img>
-          </SwiperSlide>
-        )
-      })}
+        className='mySwiper2'
+      >
+        {radioImgs.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <video id='background-video' autoPlay loop muted>
+                <source
+                  src='https://video.wixstatic.com/video/11062b_c18db2b1461b46f2ad31bae61009fee1/1080p/mp4/file.mp4'
+                  type='video/mp4'
+                />
+              </video>
+              <div className='linktestimonialslide'>
+                {/* <div className='linktestimonial'>
+                  <img src={img.pic} alt=''></img>
+                </div> */}
+                <div className='linktestimonialText'>
+                  <h2>{img.name}</h2>
+                  <h3>{img.desclink}</h3>
+                </div>
+              </div>
+
+              {/* <img src={img} alt=""></img> */}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
       <Swiper
         onSwiper={setThumbsSwiper}
@@ -42,18 +58,26 @@ const Stack = () => {
         freeMode={true}
         watchSlidesProgress={true}
         modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
+        className='mySwiper'
       >
         {radioImgs.map((img, i) => {
-        return (
-          <SwiperSlide key={i}>
-            <img src={img} alt=""></img>
-          </SwiperSlide>
-        )
-      })}
+          return (
+            <SwiperSlide key={i}>
+                <video id='background-video' autoPlay loop muted>
+                  <source
+                    src='https://video.wixstatic.com/video/11062b_c18db2b1461b46f2ad31bae61009fee1/1080p/mp4/file.mp4'
+                    type='video/mp4'
+                  />
+                </video>
+                <img src={img.pic} style={{borderRadius: '50%', zIndex: '30'}} alt=''></img>
+
+                {/* <img src={img} alt=""></img> */}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
-}
+};
 
 export default Stack;
