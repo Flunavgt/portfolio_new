19b510a637de6298e1784f20276cfc2ad24a21e8
Federico Luna <fluna1973@yahoo.com>
2023-02-06 13:49:55 -0600

    add hd pics to coworkers

--- src/Components/Stack/Images/Imgarray.jsx
+++ src/Components/Stack/Images/Imgarray.jsx
@@ -1,27 +1,27 @@
 
 const radioImgs = [
   {
-    pic: 'https://media.licdn.com/dms/image/D5635AQE3gU3fE3tvaw/profile-framedphoto-shrink_100_100/0/1674576313436?e=1676221200&v=beta&t=kcwvkBXLY4UNEK1jJYlA20GO-N5vTccF7CC-lzQodnA',
+    pic: 'https://media.licdn.com/dms/image/D5635AQE3gU3fE3tvaw/profile-framedphoto-shrink_800_800/0/1674576313436?e=1676318400&v=beta&t=t8q5qOM8uP1z_PqyFEAvS78nT4EMa_jezSp3pDUna9Y',
     name: 'Erik Sánchez Beltrán',
     desclink :'"I had the opportunity to participate with Federico at the beginning of Microverse, I discovered his excellent attitude along with many qualities that are not seen very often nowadays. Commitment, loyal to his ideals, a great resolution capacity, and effort. All this added to his kindness, and patience motivates me to highly recommend him. He is someone who doesn\'t give up, his problem-solving skills by investigating paired with his excellent eye for detail make him a unique programmer. It doesn\'t matter if he doesn\'t know a subject, give him a second and he\'ll have everything under control. Without a doubt, Federico must be part of your team."',
   },
   {
-    pic: 'https://media.licdn.com/dms/image/C4D03AQHr8oxvzo7IQg/profile-displayphoto-shrink_100_100/0/1663863384390?e=1681344000&v=beta&t=YIR4rdni3Xih7NOubyx0BxcdxWNEyYI6PZh3ftFysfE',
+    pic: 'https://media.licdn.com/dms/image/C4D03AQHr8oxvzo7IQg/profile-displayphoto-shrink_800_800/0/1663863384390?e=1681344000&v=beta&t=UU4dPj26oiZ6zJDj_zWH0UzzoH0EsvKIzjgF4laVM-s',
     name: 'Luis David Rodriguez Valades',
     desclink :'"I have had the pleasure of working with Federico for more than 6 months. After many projects together, working tirelessly, I\'m sure his knowledge, skills, and moreover, his willingness to learn, help, and finish every project have been beneficial to the team. He is self-disciplined, passionate about his work, creative, and diligent. Not only will you realize that hiring Federico is more than an eloquent decision, but you will also discover that my words are not enough to portray the human being and human quality that your company and team will have. In a few words, I have not been the same after having met Federico."',
   },
   {
-    pic: 'https://media.licdn.com/dms/image/D4D03AQGDm50BK1uaPA/profile-displayphoto-shrink_100_100/0/1664381209287?e=1681344000&v=beta&t=cPdj7x15KRIY8YMbeIPKtOq_Lwhnj7xIwpgazBeQGAo',
+    pic: 'https://media.licdn.com/dms/image/D4D35AQEDS_2WL_Z2Og/profile-framedphoto-shrink_800_800/0/1664381213687?e=1676318400&v=beta&t=RBk5Ov5CAfNOCk0NKms3H2F5jKyCdUgl9X_6oW5jjaE',
     name: 'Addis Tsega',
     desclink: 'We\'ve joined our hands on several projects, and Federico is one of the best people I had as a partner. I highly recommend his expertise to any organization looking for a Full-stack developer. He is the most profound person I have met, and his ability to tackle any problem is remarkable and with a warm smile. Federico would become an appreciated member of any team.',
   },
   {
-    pic: 'https://media.licdn.com/dms/image/C4D03AQG1-SNI18A4_g/profile-displayphoto-shrink_100_100/0/1663930572336?e=1681344000&v=beta&t=dbDXr6h2NsMRgx9M6B5-LkJfdN__d-XDZ18vWQpoGA4',
+    pic: 'https://media.licdn.com/dms/image/D4D35AQHU-3fiL5Qeuw/profile-framedphoto-shrink_400_400/0/1664543914263?e=1676318400&v=beta&t=6EseKVXW6d6oCJFm-Iihl5myxImsMXRDv2J_6vBuDL4',
     name: 'Abdelrhman Samy',
     desclink :'"Federico is one of the brightest software developers I\'ve ever met. I worked alongside him in Microverse, and during that time he proved his commitment and determination over and over again. He is always challenging himself--he\'s the type of person who reads documentation for fun, and his attention to detail is evident in all of the projects I\'ve worked with him on. He\'s also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend him without reservation."',
   },
   {
-    pic: 'https://media.licdn.com/dms/image/D4D03AQFuvBfoxQQK4Q/profile-displayphoto-shrink_100_100/0/1665264186231?e=1681344000&v=beta&t=_r5tRFKaynKHhgepbzFSrEIYRJIMIGgKhVBmOeNXmuc',
+    pic: 'https://media.licdn.com/dms/image/D4D35AQHmud0MhhAU4w/profile-framedphoto-shrink_800_800/0/1665264187389?e=1676318400&v=beta&t=TaU0TuaGYISX8X0fNR7Cg7AJ6NHrqe56FA_2OIDv7bA',
     name: 'Fabio Lima',
     desclink: '"I got the opportunity to learn and speak with Federico on several occasions. He is knowledgeable, diligent, and smart. He makes every attempt to assist others while also seeking to learn from his colleagues whenever possible. His commitment and motivation are admirable since he is constantly looking for opportunities to grow and learn new things. Federico is the sort of individual that strives to excel and would be a wonderful addition to any team."',
   },
